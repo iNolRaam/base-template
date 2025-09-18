d80a5e158446e93036419a95184aecb0c0c71a8d
Marlon Castillo <aac.marlondv@gmail.com>
2025-09-17 21:03:57 -0600

    Refactor BaseRepositoryTest and application-test.yml for improved database configuration and initialization

--- src/test/java/com/inolraam/basetemplate/adapter/out/jpa/repository/BaseRepositoryTest.java
+++ src/test/java/com/inolraam/basetemplate/adapter/out/jpa/repository/BaseRepositoryTest.java
@@ -3,35 +3,13 @@ package com.inolraam.basetemplate.adapter.out.jpa.repository;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(scripts = "classpath:DDL/schema.sql")
 @ActiveProfiles("test")
 public abstract class BaseRepositoryTest {
 
-    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
 
-    static {
-        POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:15.2")
-                .withDatabaseName("testdb")
-                .withUsername("test")
-                .withPassword("test");
-        POSTGRES_CONTAINER.start();
-    }
-
-    @DynamicPropertySource
-    static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
-        registry.add("spring.sql.init.mode", () -> "always");
-    }
 }
